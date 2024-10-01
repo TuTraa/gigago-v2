06b900b5e29550f7f9fd8ebb666e9b4ca6b2fb8a
jiminsample1 <jiminsample1@gmail.com>
2024-10-01 22:07:13 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-export default function Home({ children }: { children }) {
+export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
@@ -14,7 +14,9 @@ export default function Home({ children }: { children }) {
         />
         <ol className='list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
           <li className='mb-2'>GIGAGO V2 by Next.js</li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly.
+
+</li>
         </ol>
 
         <div className='flex gap-4 items-center flex-col sm:flex-row'>
