e8a2c54b8315cf9d45b61aa9f41c5359c5ebad8f
Hoang Tuan Anh <hoanganhss2k@gmail.com>
2024-10-02 14:00:09 +0700

    feat(config): add check husky commit rule

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,52 +1,9 @@
 import type { UserConfig } from '@commitlint/types'
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  // parserPreset: 'conventional-changelog-atom',
+  parserPreset: 'conventional-changelog-atom',
   formatter: '@commitlint/format',
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^\[(.*?)\] - (.*?):(.*),\s*jira_id:\s*#(\d+)/,
-      headerCorrespondence: ['platform', 'type', 'subject', 'jira_id'],
-    },
-  },
-  plugins: [
-    {
-      rules: {
-        'header-match-team-pattern': (parsed) => {
-          const { platform, type, subject, jira_id } = parsed
-          if (
-            platform === null &&
-            type === null &&
-            subject === null &&
-            jira_id === null
-          ) {
-            return [
-              false,
-              "\x1b[31mERROR\x1b[0m: Please follow the format '[cloud] - [chore] subject, rm_id:#RM_ID'",
-            ]
-          }
-          return [true, '']
-        },
-        'gitmoji-type-enum': (parsed, _when, expectedValue) => {
-          const platformType = ['Cloud', 'Development', 'QA'] //change your platforms here
-          const { type, platform } = parsed
-          if (type && !expectedValue.includes(type)) {
-            return [
-              false,
-              `\x1b[31mERROR\x1b[0m: [${type}] doesn't include in [${expectedValue}]`,
-            ]
-          }
-          if (platform && !platformType.includes(platform)) {
-            return [
-              false,
-              `\x1b[31mERROR\x1b[0m: [${platform}] doesn't include in [${platformType}]`,
-            ]
-          }
-          return [true, '']
-        },
-      },
-    },
-  ],
+
   rules: {
     'type-enum': [
       2,
