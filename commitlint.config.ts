f8f256d0886926b8517d14573ae87861a7a15c71
Hoang Tuan Anh <hoanganhss2k@gmail.com>
2024-10-02 11:28:47 +0700

    Iam testing if its working with wrong pattern

---
+++ commitlint.config.ts
@@ -0,0 +1,45 @@
+import type { UserConfig } from '@commitlint/types'
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-atom',
+  formatter: '@commitlint/format',
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // new feature
+        'fix', // bug fix
+        'docs', // documentation changes
+        'style', // changes that do not affect the meaning of the code (white-space, formatting, etc.)
+        'refactor', // code changes that neither fix a bug or add a feature
+        'perf', // performance improvement
+        'test', // adding missing tests or correcting existing tests
+        'build', // changes that affect the build system or external dependencies (example scopes: npm)
+        'ci', // changes to CI configuration files and scripts
+        'chore', // other changes that don't modify src or test files
+        'revert', // reverts a previous commit
+      ],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'setup', // project setup
+        'config', // configuration files
+        'deps', // dependency updates
+        'feature', // feature-specific changes
+        'bug', // bug fixes
+        'docs', // documentation
+        'style', // code style/formatting
+        'refactor', // code refactoring
+        'test', // tests
+        'build', // build scripts or configuration
+        'ci', // continuous integration
+        'release', // release related changes
+        'other', // other changes
+      ],
+    ],
+  },
+}
+export default Configuration
